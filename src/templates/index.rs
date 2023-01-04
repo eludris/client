0c26424c33bfb88db25775eb2d9759f666423900
Enoki <enokiun@gmail.com>
2023-01-04 21:25:03 +0300

    feat: add the ability to send messages

--- src/templates/index.rs
+++ src/templates/index.rs
@@ -1,9 +1,13 @@
+use std::rc::Rc;
+
 use crate::global_state::AppStateRx;
 use perseus::prelude::*;
 use sycamore::prelude::*;
+use todel::models::Message;
 
 pub fn index_page<G: Html>(cx: Scope) -> View<G> {
     let state = Reactor::<G>::from_cx(cx).get_global_state::<AppStateRx>(cx);
+    let content = create_signal(cx, String::new());
 
     #[cfg(client)]
     {
@@ -12,16 +16,50 @@ pub fn index_page<G: Html>(cx: Scope) -> View<G> {
     }
 
     view! { cx,
-        ul(class = "messages") {
-            Indexed(
-                iterable = &state.messages,
-                view = |cx, x| view! { cx,
-                    div(class = "message") {
-                        span(class = "author") { (format!("[{}]: ", x.author)) }
-                        span(class = "content") { (x.content) }
+        div(class = "message-channel")
+        {
+            ul(id = "messages") {
+                Indexed(
+                    iterable = &state.messages,
+                    view = |cx, x| view! { cx,
+                        div(class = "message") {
+                            span(class = "author") { (format!("[{}]: ", x.author)) }
+                            span(class = "content") { (x.content) }
+                        }
+                    }
+                )
+            }
+            form(
+                id = "message-input-form",
+                on:submit = move |e| {
+                    #[cfg(client)]
+                    {
+                        use web_sys::Event;
+                        use gloo_net::http::Request;
+
+                        let e: Event = e;
+                        e.prevent_default();
+                        spawn_local_scoped(cx, async {
+                            Request::post("https://eludris.tooty.xyz/messages")
+                                .json(&Message {
+                                    author: state.name.get().as_ref().clone().unwrap(),
+                                    content: Rc::try_unwrap(content.take()).unwrap(),
+                                })
+                                .unwrap()
+                                .send()
+                                .await
+                                .unwrap();
+                        })
                     }
                 }
-            )
+            ) {
+                input(
+                    id = "message-input",
+                    placeholder = "Send a message to Eludris",
+                    bind:value = content
+                ) {}
+                button(id = "send-button") { "Send" }
+            }
         }
     }
 }
