7113f657fe1f5d53bb5d5b91aa914e15bff8b323
Enoki <enokiun@gmail.com>
2023-01-08 15:18:58 +0300

    chore: format code

--- src/error_views.rs
+++ src/error_views.rs
@@ -5,8 +5,8 @@ pub fn get_error_views<G: Html>() -> ErrorViews<G> {
     ErrorViews::new(|cx, err, _, _| {
         (
             view! { cx,
-            title { "Errow" }
-                },
+                title { "Errow" }
+            },
             view! { cx,
                 h1 { "It seems that shit went down, welp." }
                 p { (format!("Error {}", err)) }
