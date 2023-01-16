fbaafa3a5fc3eca8b86250cbc1ecbfec84d4d60f
Enoki <enokiun@gmail.com>
2023-01-17 02:07:23 +0300

    feat: add validation to login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,17 +5,43 @@
 
   let value = '';
   let instanceURL = '';
+  let error = '';
 
   onMount(() => {
     if ($data) goto('/');
   });
 
   const onSubmit = () => {
-    data.set({
-      name: value,
-      instanceURL: instanceURL ? instanceURL : 'https://eludris.tooty.xyz'
-    });
-    goto('/');
+    if (!error && value) {
+      const url: string = instanceURL.startsWith('http') ? instanceURL : 'https://' + instanceURL; // I could one line this but I like my codebases sane
+      data.set({
+        name: value,
+        instanceURL: instanceURL ? url : 'https://eludris.tooty.xyz'
+      });
+      goto('/');
+    }
+  };
+
+  const onUsernameKeyUp = () => {
+    if (value.length < 2 || value.length > 32) {
+      error = 'Your username must be between 2 and 32 characters in length';
+    } else {
+      error = '';
+    }
+  };
+
+  const onURLKeyUp = () => {
+    if (!instanceURL) {
+      error = '';
+      return;
+    }
+    const url: string = instanceURL.startsWith('http') ? instanceURL : 'https://' + instanceURL;
+    try {
+      new URL(url);
+      error = '';
+    } catch {
+      error = 'Invalid instance url';
+    }
   };
 </script>
 
@@ -23,9 +49,17 @@
   <form id="login-form" on:submit|preventDefault={onSubmit}>
     <h1>Log in to Eludris</h1>
     <label for="username">Username</label>
-    <input bind:value name="username" />
+    <input on:keyup={onUsernameKeyUp} bind:value name="username" placeholder="Username" />
     <label for="instanceUrl">Instance URL</label>
-    <input bind:value={instanceURL} name="instanceUrl" placeholder="https://eludris.tooty.xyz" />
-    <button type="submit">Log in</button>
+    <input
+      on:keyup={onURLKeyUp}
+      bind:value={instanceURL}
+      name="instanceUrl"
+      placeholder="https://eludris.tooty.xyz"
+    />
+    {#if error}
+      <span class="error">{error}</span>
+    {/if}
+    <button type="submit" disabled={!!error}>Log in</button>
   </form>
 </div>
