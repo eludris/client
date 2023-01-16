b03858c95e0fd8619040be365bb001abc87c8b32
Enoki <enokiun@gmail.com>
2023-01-16 20:42:17 +0300

    feat: add websocket logic

---
+++ src/lib/ws.ts
@@ -0,0 +1,44 @@
+import { browser } from '$app/environment';
+import { writable } from 'svelte/store';
+import data from '$lib/data';
+import type { Message } from '$lib/types/message';
+import type { InstanceInfo } from '$lib/types/instance';
+import { PayloadOP, type IncomingPayload } from '$lib/types/event';
+
+const messages = writable<Array<Message>>([]);
+
+let instanceUrl: string | null = null;
+let ws: WebSocket | null = null;
+let pingInterval: NodeJS.Timer | null = null;
+
+if (browser) {
+  data.subscribe(async (value) => {
+    if (value) {
+      if (value.instanceURL != instanceUrl) {
+        const res = await fetch(value.instanceURL);
+        const info: InstanceInfo = await res.json();
+
+        ws = new WebSocket(info.pandemonium_url);
+
+        pingInterval = setInterval(() => ws?.send(JSON.stringify({ op: PayloadOP.PING })), 45_000);
+
+        ws?.addEventListener('message', (msg: MessageEvent) => {
+          const payload: IncomingPayload = JSON.parse(msg.data);
+
+          if (payload.op == PayloadOP.MESSAGE_CREATE)
+            messages.update((messages) => {
+              messages.push(payload.d);
+              return messages;
+            });
+        });
+      }
+    } else {
+      instanceUrl = null;
+      ws?.close();
+      if (pingInterval) clearInterval(pingInterval);
+      ws = null;
+    }
+  });
+}
+
+export default messages;
