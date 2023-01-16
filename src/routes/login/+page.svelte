d271f7a6eaff312cafdd47abc6a33f7896f66a55
Enoki <enokiun@gmail.com>
2023-01-16 20:44:49 +0300

    feat: improve login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,25 +2,28 @@
   import { onMount } from 'svelte';
   import { goto } from '$app/navigation';
   import data from '$lib/data';
-  let input: HTMLInputElement;
+
+  let value = '';
+  let instanceURL = '';
 
   onMount(() => {
     if ($data) goto('/');
   });
 
-  const onSubmit = (e: Event) => {
-    e.preventDefault();
+  const onSubmit = () => {
     data.set({
-      name: input.value,
-      instanceURL: 'https://eludris.tooty.xyz'
+      name: value,
+      instanceURL: instanceURL ? instanceURL : 'https://eludris.tooty.xyz'
     });
     goto('/');
   };
 </script>
 
-<h1>log in or else</h1>
-
-<form on:submit={onSubmit}>
-  <label for="username"> Username </label>
-  <input bind:this={input} name="username" />
+<form id="login-form" on:submit|preventDefault={onSubmit}>
+  <h1>log in or else</h1>
+  <label for="username">Username</label>
+  <input bind:value name="username" />
+  <label for="instanceUrl">Instance URL</label>
+  <input bind:value={instanceURL} name="instanceUrl" />
+  <button type="submit">Log In</button>
 </form>
