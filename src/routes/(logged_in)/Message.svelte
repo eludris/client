24aed97d88486f838af605d27e45ee2c56215ba3
Enoki <enokiun@gmail.com>
2023-01-17 21:50:12 +0300

    feat: refactor Message into it's own component

---
+++ src/routes/(logged_in)/Message.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Message } from '$lib/types/message';
+  import Markdown from '$lib/components/Markdown.svelte';
+
+  export let message: Message;
+  export let showAuthor: boolean;
+</script>
+
+<div class="message">
+  {#if showAuthor}
+    <div class="author">{message.author}</div>
+  {/if}
+  <div class="content"><Markdown content={message.content} /></div>
+</div>
+
+<style>
+  .content {
+    margin: 0;
+    margin-left: 20px;
+  }
+
+  .author {
+    margin: 10px 0;
+  }
+</style>
