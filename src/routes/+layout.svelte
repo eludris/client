bdca07323d18cb66f4538370dd5ec825adc9b968
Enoki <enokiun@gmail.com>
2023-01-17 22:30:38 +0300

    feat: apply user custom styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import data from '$lib/data';
+</script>
+
+<svelte:head>
+  {#if $data?.styles}
+    {#if $data.styles.indexOf('</styles>') == -1}
+      <!-- poor man's sanitization /shrug -->
+      {@html `<style>${$data.styles}</style>`}
+    {/if}
+  {/if}
+</svelte:head>
+
+<slot />
