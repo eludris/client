4fe80fc731d285b1d91d7da42a6ffed755de6587
Enoki <enokiun@gmail.com>
2023-01-17 22:31:28 +0300

    feat: add initial settings page

---
+++ src/routes/(logged_in)/settings/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import data from '$lib/data';
+</script>
+
+<a href="/">Back</a>
+{#if $data}
+  <input bind:value={$data.name} />
+  <textarea bind:value={$data.styles} />
+{/if}
