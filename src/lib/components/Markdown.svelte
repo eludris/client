ec9aa6b1d4ef9fb8364b9e7d1af2dc33a455427a
Enoki <enokiun@gmail.com>
2023-01-20 22:20:19 +0300

    feat: improve Markdown component
    closes #10

--- src/lib/components/Markdown.svelte
+++ src/lib/components/Markdown.svelte
@@ -1,104 +1,48 @@
-<script lang="ts" context="module">
-  import { unified, type Plugin } from 'unified';
-  import remarkParse from 'remark-parse';
-  import remarkRehype from 'remark-rehype';
-  import remarkBreaks from 'remark-breaks';
-  import remarkMath from 'remark-math';
-  import remarkGFM from 'remark-gfm';
-  import rehypeKatex from 'rehype-katex';
-  import rehypePrism from 'rehype-prism';
-  import rehypeStringify from 'rehype-stringify';
-
-  import { visit } from 'unist-util-visit';
-
-  const remarkTextifyHtml: Plugin = () => {
-    return (tree) =>
-      visit(tree, 'html', (node: { type: string }) => {
-        node.type = 'text';
-      });
-  };
-
-  const remarkKillImages: Plugin = () => {
-    return (tree) =>
-      visit(tree, 'image', (node: { type: string; alt: string; value: string }) => {
-        node.type = 'text';
-        node.value = node.alt;
-      });
-  };
-
-  const remarkExternalAnchors: Plugin = () => {
-    return (tree) =>
-      visit(tree, 'element', (node: { tagName: string; properties: { target: string } }) => {
-        if (node.tagName == 'a') {
-          node.properties.target = '_blank';
-        }
-      });
-  };
-
-  const unScrewHtml = (
-    html: string
-  ): string => // I've spent so much time trying to fix this, thanks to revolt I finally managed to get it working
-    html
-      .replace(/^(<\/?[a-zA-Z0-9]+>)(.*$)/gm, '\u200E$&')
-      // force whitespace for blockquotes
-      .replace(/^([^\\]|)\>[^>\s]/gm, '\\$&')
-      // ensure ``` s have a new line before them
-      .replace(new RegExp('(.)```', 'gm'), '$1\n```')
-      // ... and after them
-      .replace(new RegExp('``` (.)', 'gm'), '```\n$1');
-
-  const renderer = unified()
-    .use(remarkParse)
-    .use(remarkBreaks)
-    .use(remarkMath)
-    .use(remarkTextifyHtml)
-    .use(remarkKillImages)
-    .use(remarkGFM, {
-      singleTilde: false
-    })
-    .use(remarkRehype)
-    .use(rehypeStringify)
-    .use(rehypeKatex, { trust: false, strict: false, output: 'html', throwOnError: false })
-    .use(remarkExternalAnchors)
-    .use(rehypePrism);
-</script>
-
 <script lang="ts">
   import { browser } from '$app/environment';
-  import Prism from 'prismjs';
+  import markdown from '$lib/markdown';
 
   if (browser) {
     try {
-      // @ts-expect-error: this is untyped :(
-      import('prismjs/components/prism-rust.js');
-      // @ts-expect-error: this is untyped :(
-      import('prismjs/components/prism-python.js');
-      // @ts-expect-error: this is untyped :(
-      import('prismjs/components/prism-typescript.js');
-      // @ts-expect-error: this is untyped :(
-      import('prismjs/components/prism-nasm.js');
-      // @ts-expect-error: this is untyped :(
-      import('prismjs/components/prism-go.js');
-      Prism.languages['rs'] = Prism.languages['rust'];
+      import('prismjs').then((Prism) => {
+        // @ts-expect-error: this is untyped :(
+        import('prismjs/components/prism-rust.js');
+        // @ts-expect-error: this is untyped :(
+        import('prismjs/components/prism-python.js');
+        // @ts-expect-error: this is untyped :(
+        import('prismjs/components/prism-typescript.js');
+        // @ts-expect-error: this is untyped :(
+        import('prismjs/components/prism-nasm.js');
+        // @ts-expect-error: this is untyped :(
+        import('prismjs/components/prism-go.js');
+        Prism.languages['rs'] = Prism.languages['rust'];
+      });
     } catch (e) {
       console.error('Could not load prism syntax highlighting');
     }
   }
 
   export let content: string;
+  export let preRendered = false;
 </script>
 
-{#await renderer.process(unScrewHtml(content))}
-  <span> Loading markdown </span>
-{:then content}
+{#if preRendered}
   <div class="md">
     {@html content}
   </div>
-{:catch e}
-  <span style:color="red">
-    Error loading markdown {e}
-  </span>
-{/await}
+{:else}
+  {#await markdown(content)}
+    <span> Loading markdown </span>
+  {:then content}
+    <div class="md">
+      {@html content}
+    </div>
+  {:catch e}
+    <span style:color="red">
+      Error loading markdown {e}
+    </span>
+  {/await}
+{/if}
 
 <svelte:head>
   <link
