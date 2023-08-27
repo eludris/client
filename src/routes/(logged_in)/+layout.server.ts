094cb6da61a7bd4d85227d409883a84943b1bb99
Enoki <enokiun@gmail.com>
2023-08-27 22:48:04 +0300

    feat: add more fun fun facts

--- src/routes/(logged_in)/+layout.server.ts
+++ src/routes/(logged_in)/+layout.server.ts
@@ -10,9 +10,11 @@ const facts = [
   'Eludris is factually better than Disco- well, maybe not yet',
   'I am the storm that is approaching',
   'Coffee is kinda pog',
-  'The name pengin originally started as a joke about linux users while watching <a href="https://cdn.eludris.gay/static/pengin.mp4" target="_blank">this video</a> on loop',
+  'The name pengin (old client name) originally started as a joke about linux users while watching <a href="https://cdn.eludris.gay/static/pengin.mp4" target="_blank">this video</a> on loop',
   'ggVG is a sensible keybind.',
   'It\'s been a long night and I can feel your anger',
+  'wei wei wei wei',
+  'The only thing that you gotta know is that I do what it takes do what it takes!',
 ];
 
 export function load() {
