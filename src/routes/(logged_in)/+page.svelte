91971f38b9be423d5b39c84ca1b7c589cdaf423c
Enoki <enokiun@gmail.com>
2023-09-19 23:53:59 +0300

    feat: add icons for buttons

--- src/routes/(logged_in)/+page.svelte
+++ src/routes/(logged_in)/+page.svelte
@@ -186,8 +186,16 @@
         </span>
       {/if}
     </div>
-    <a id="settings-link" href="/settings"> Settings </a>
     <button id="logout-button" on:click={logOut}> Logout </button>
+    <a id="settings-link" href="/settings">
+      <!--- https://icon-sets.iconify.design/mdi/cog/ --->
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+        ><path
+          fill="currentColor"
+          d="M12 15.5A3.5 3.5 0 0 1 8.5 12A3.5 3.5 0 0 1 12 8.5a3.5 3.5 0 0 1 3.5 3.5a3.5 3.5 0 0 1-3.5 3.5m7.43-2.53c.04-.32.07-.64.07-.97c0-.33-.03-.66-.07-1l2.11-1.63c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.31-.61-.22l-2.49 1c-.52-.39-1.06-.73-1.69-.98l-.37-2.65A.506.506 0 0 0 14 2h-4c-.25 0-.46.18-.5.42l-.37 2.65c-.63.25-1.17.59-1.69.98l-2.49-1c-.22-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64L4.57 11c-.04.34-.07.67-.07 1c0 .33.03.65.07.97l-2.11 1.66c-.19.15-.25.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1.01c.52.4 1.06.74 1.69.99l.37 2.65c.04.24.25.42.5.42h4c.25 0 .46-.18.5-.42l.37-2.65c.63-.26 1.17-.59 1.69-.99l2.49 1.01c.22.08.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.66Z"
+        /></svg
+      >
+    </a>
   </div>
   <div id="channel-view">
     <div id="message-channel-body" class={!$userConfig.userList ? 'user-hidden' : ''}>
@@ -204,7 +212,12 @@
           on:submitFiles={submitFiles}
           scrollContainer={messagesUList}
         />
-        <button id="send-button">Send</button>
+        <button id="send-button">
+          <!--- https://icon-sets.iconify.design/mdi/send/ --->
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+            ><path fill="currentColor" d="m2 21l21-9L2 3v7l15 2l-15 2v7Z" /></svg
+          >
+        </button>
       </form>
     </div>
     {#if !$userConfig.userList}
@@ -287,6 +300,8 @@
     font-size: inherit;
     transition: color ease-in-out 125ms;
     cursor: pointer;
+    height: 32px;
+    padding-top: 6px;
   }
 
   #send-button:hover {
@@ -337,6 +352,7 @@
     background-color: var(--gray-200);
     padding: 10px;
     gap: 20px;
+    align-items: baseline;
   }
 
   #instance-info {
@@ -377,24 +393,7 @@
     text-decoration: none;
     align-self: center;
     border: unset;
-  }
-
-  #logout-button {
-    padding: 5px 10px;
-    outline: none;
-    border: unset;
-    border-radius: 25px;
-    background-color: var(--pink-500);
-    color: var(--purple-100);
-    box-shadow: 0 2px 4px var(--purple-200);
-    transition: box-shadow ease-in-out 200ms, color ease-in-out 200ms,
-      background-color ease-in-out 200ms;
-    cursor: pointer;
-  }
-
-  #logout-button:hover {
-    box-shadow: 0 5px 20px var(--purple-200);
-    background-color: var(--pink-600);
+    height: 24px;
   }
 
   @media only screen and (max-width: 1000px) {
