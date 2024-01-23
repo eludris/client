44fa0cca521ae0d43da2b42582621723f4e11203
Enoki <enokiun@gmail.com>
2024-01-23 20:50:28 +0300

    fix: unbreak Hakkude's commit

--- src/routes/(logged_in)/+layout.server.ts
+++ src/routes/(logged_in)/+layout.server.ts
@@ -24,7 +24,7 @@ const facts = [
   'Bob Bobster',
   '<span style="background-color: hsl(235 calc(1*85.6%) 64.7%/0.3); padding: 0 2px; border-radius: 3px;">@Duffelbag</span> eval',
   'One two three four five once I caught a fish alive',
-  "There's a secret magical hidden /setting/reset-theme route for when the epic theme from untrusted sources you install makes your client unusable!".
+  "There's a secret magical hidden /setting/reset-theme route for when the epic theme from untrusted sources you install makes your client unusable!",
 ];
 
 export function load() {
