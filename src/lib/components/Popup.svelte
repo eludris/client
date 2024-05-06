fbfe18855b4a4c1a443f3f11e6bbf3e073119bf6
Chromosomologist <chromosomologist@gmail.com>
2024-05-06 12:10:50 +0200

    fix: don't close window on mouseup

--- src/lib/components/Popup.svelte
+++ src/lib/components/Popup.svelte
@@ -27,7 +27,7 @@
 <svelte:head><style>#app {overflow: hidden; user-select: none;}</style></svelte:head>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div id="popup-container" on:click={containerClick}>
+<div id="popup-container" on:mousedown={containerClick}>
   <div id="popup" bind:this={popup}>
     <h2 id="popup-title"><slot name="title">Notice</slot></h2>
     <span id="popup-message"><slot /></span>
