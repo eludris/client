9d6378fa2c022d42d022b98d80cc8054f6bcdcff
Chromosomologist <chromosomologist@gmail.com>
2024-05-17 10:00:43 +0200

    feat: actually read documentation
    https://github.com/Menci/vite-plugin-wasm?tab=readme-ov-file#web-worker
    cloudflare pls work thx

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 const config: UserConfig = {
-  plugins: [sveltekit(), wasm()]
+  plugins: [sveltekit(), wasm(), topLevelAwait()],
+  worker: {
+    plugins: [wasm(), topLevelAwait()]
+  }
 };
 
 export default config;
