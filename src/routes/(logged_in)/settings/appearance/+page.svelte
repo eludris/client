78c2bf4b9da159eed57e484b18bc4755c1e9f21e
Chromosomologist <chromosomologist@gmail.com>
2024-06-06 01:13:32 +0200

    feat: implement theme store changes

--- src/routes/(logged_in)/settings/appearance/+page.svelte
+++ src/routes/(logged_in)/settings/appearance/+page.svelte
@@ -44,12 +44,12 @@
   }
 
   const fetchTheme = async (theme: ThemeInfo) => {
-    if (theme['readme-url']) {
-      themeDesc = await fetch(theme['readme-url']).then((r) => r.text())
+    if (theme.readme_url) {
+      themeDesc = await fetch(theme.readme_url).then((r) => r.text())
     } else {
       // Assume github repo with main branch 'main' and readme file 'README.md'.
       themeDesc = await fetch(
-        `${repoToRaw(theme['repo-url'])}/main/README.md`
+        `${repoToRaw(theme.repo_url)}/main/README.md`
       ).then((r) => r.text());
     }
 
@@ -67,12 +67,12 @@
   }
 
   const useTheme = async () => {
-    if (selectedTheme!['style-url']) {
-      $config.styles = styles = await fetch(selectedTheme!['style-url']!).then((r) => r.text())
+    if (selectedTheme!.style_url) {
+      $config.styles = styles = await fetch(selectedTheme!.style_url!).then((r) => r.text())
     } else {
       // Assume github repo with main branch 'main' and style file 'style.css'.
       $config.styles = styles = await fetch(
-        `${repoToRaw(selectedTheme!['repo-url'])}/main/style.css`
+        `${repoToRaw(selectedTheme!.repo_url)}/main/style.css`
       ).then((r) => r.text());
     }
   } 
@@ -90,7 +90,7 @@
         role="button"
         tabindex=0
       >
-        <img class="theme-cover" src={theme['cover-image']} alt="Theme cover">
+        <img class="theme-cover" src={theme.cover_url} alt="Theme cover">
         <div class="theme-header">
           <div class="theme-name">{theme.name}</div>
           <div class="theme-author">by {theme.author}</div>
@@ -129,7 +129,7 @@
     </div>
     <span slot="control">
       <div class="theme-buttons">
-        <a class="theme-button repo" href={selectedTheme?.['repo-url']} title="View theme repository">
+        <a class="theme-button repo" href={selectedTheme?.repo_url} title="View theme repository">
           <svg viewBox="0 0 24 24" height=24 width=24>
             <path fill="currentColor" fill-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" clip-rule="evenodd"></path>
           </svg>
--- src/routes/(logged_in)/settings/appearance/+page.ts
+++ src/routes/(logged_in)/settings/appearance/+page.ts
@@ -3,18 +3,17 @@ import type { PageLoad } from './$types';
 type ThemeInfo = {
   name: string;
   author: string;
-  'repo-url': string;
   description: string;
-  'cover-image': string;
-  'readme-url': string | undefined;
-  'style-url': string | undefined;
+  cover_url: string;
+  repo_url: string;
+  readme_url: string | undefined;
+  style_url: string | undefined;
 };
 
 export const load: PageLoad = async ({ fetch, params }): Promise<{theme_info: ThemeInfo[]}> => {
   let themeInfo: ThemeInfo[] = await fetch("https://raw.githubusercontent.com/eludris/client-themes/main/themes.json").then((r) => r.json());
 
   // NOTE: these are temporary, for testing.
-  themeInfo[0].author = "Sharp Eyes"
   themeInfo = [...themeInfo, ...themeInfo, ...themeInfo, ...themeInfo, ...themeInfo, ...themeInfo];
 
 	return {theme_info: themeInfo};
