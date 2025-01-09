458007d5322106e2ba3ef8e5bfdc717b080c0559
Chromosomologist <chromosomologist@gmail.com>
2025-01-09 15:48:32 +0100

    feat: do some stupid hover shit

--- src/routes/(logged_in)/settings/sessions/+page.svelte
+++ src/routes/(logged_in)/settings/sessions/+page.svelte
@@ -2,6 +2,8 @@
     import { request } from "$lib/request";
     import type { Session } from "$lib/types/session";
 
+    let hoverClass = "";
+
     const PLATFORM_ICONS = {
         windows: "M3 12V6.75l6-1.32v6.48zm17-9v8.75l-10 .15V5.21zM3 13l6 .09v6.81l-6-1.15zm17 .25V22l-10-1.91V13.1z",
         linux: "M14.62 8.35c-.42.28-1.75 1.04-1.95 1.19c-.39.31-.75.29-1.14-.01c-.2-.16-1.53-.92-1.95-1.19c-.48-.31-.45-.7.08-.92c1.64-.69 3.28-.64 4.91.03c.49.21.51.6.05.9m7.22 7.28c-.93-2.09-2.2-3.99-3.84-5.66a4.3 4.3 0 0 1-1.06-1.88c-.1-.33-.17-.67-.24-1.01c-.2-.88-.29-1.78-.7-2.61c-.73-1.58-2-2.4-3.84-2.47c-1.81.05-3.16.81-3.95 2.4c-.21.43-.36.88-.46 1.34c-.17.76-.32 1.55-.5 2.32c-.15.65-.45 1.21-.96 1.71c-1.61 1.57-2.9 3.37-3.88 5.35c-.14.29-.28.58-.37.88c-.19.66.29 1.12.99.96c.44-.09.88-.18 1.3-.31c.41-.15.57-.05.67.35c.65 2.15 2.07 3.66 4.24 4.5c4.12 1.56 8.93-.66 9.97-4.58c.07-.27.17-.37.47-.27c.46.14.93.24 1.4.35c.49.09.85-.16.92-.64c.03-.26-.06-.49-.16-.73",
@@ -28,28 +30,39 @@
         console.log(id)
         return new Date((id >>> 16) + ELUDRIS_EPOCH);
     }
+
+    const hoverThing = (e: MouseEvent, expand: boolean) => {
+        console.log(e.target);
+        if (expand) {
+            (e.currentTarget as SVGElement).classList.add("expand");
+            (e.currentTarget as SVGElement).nextElementSibling?.classList.add("retract");
+            (e.currentTarget as SVGElement).previousElementSibling?.classList.add("retract");
+        } else {
+            (e.currentTarget as SVGElement).classList.remove("expand", "retract");
+            (e.currentTarget as SVGElement).nextElementSibling?.classList.remove("expand", "retract");
+            (e.currentTarget as SVGElement).previousElementSibling?.classList.remove("expand", "retract");
+        }
+    }
 </script>
 
 
 {#await fetch_sessions()}
     <h4>Doodoo</h4>
 {:then sessions}
-    <!-- <table>
-        <tr>
-            <td>IP Address</td>
-            <td>Platform</td>
-            <td>Client</td>
-        </tr> -->
     <div class="sessions">
         {#each sessions as session (session.id)}
             <div class="session-container">
                 <div class="session-icon-wrapper">
-                    <svg class="platform-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path fill="currentColor" d={PLATFORM_ICONS[session.platform] ?? UNKNOWN_ICON}/>
-                    </svg>
-                    <svg class="client-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path fill="currentColor" d={CLIENT_ICONS[session.client] ?? UNKNOWN_ICON}/>
-                    </svg>
+                    <div class={"platform-icon"} on:mouseover={(e) => hoverThing(e, true)} on:mouseout={(e) => hoverThing(e, false)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="currentColor" d={PLATFORM_ICONS[session.platform] ?? UNKNOWN_ICON}/>
+                        </svg>
+                    </div>
+                    <div class={"client-icon"} on:mouseover={(e) => hoverThing(e, true)} on:mouseout={(e) => hoverThing(e, false)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="currentColor" d={CLIENT_ICONS[session.client] ?? UNKNOWN_ICON}/>
+                        </svg>
+                    </div>
                 </div>
                 <div class="session-data">
                     <h3>{`${session.client} on ${session.platform}`}</h3>
@@ -80,10 +93,6 @@
         gap: 5px;
     }
 
-    .session-icon-container {
-        position: relative;
-    }
-
     .session-icon-wrapper {
         position: relative;
         height: 60px;
@@ -95,6 +104,23 @@
         clip-path: polygon(0 0, calc(66% - 2px) 0, calc(33% - 2px) 100%, 0 100%);
         height: 100%;
         width: 100%;
+        transition: clip-path 0.5s ease-in-out;
+    }
+
+    .platform-icon svg {
+        height: 100%;
+        width: 100%;
+        pointer-events: none;
+    }
+
+    :global(.platform-icon.expand) {
+        clip-path: polygon(0 0, 133% 0, 100% 100%, 0 100%) !important;
+        transition: clip-path 0.5s ease-in-out;
+    }
+
+    :global(.platform-icon.retract) {
+        clip-path: polygon(-66% 0, 0 0, -33% 100%, -66% 100%) !important;
+        transition: clip-path 0.5s ease-in-out;
     }
 
     .client-icon {
@@ -102,6 +128,23 @@
         clip-path: polygon(calc(66% + 2px) 0, 100% 0, 100% 100%, calc(33% + 2px) 100%);
         height: 100%;
         width: 100%;
+        transition: clip-path 0.5s ease-in-out;
+    }
+
+    .client-icon svg {
+        height: 100%;
+        width: 100%;
+        pointer-events: none;
+    }
+
+    :global(.client-icon.expand) {
+        clip-path: polygon(0 0, 100% 0, 100% 100%, -33% 100%) !important;
+        transition: clip-path 0.5s ease-in-out;
+    }
+
+    :global(.client-icon.retract) {
+        clip-path: polygon(133% 0, 166% 0, 166% 100%, 100% 100%) !important;
+        transition: clip-path 0.5s ease-in-out;
     }
 
     h3 {
